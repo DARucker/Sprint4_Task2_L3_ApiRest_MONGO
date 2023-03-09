29ab5a3ad732703e3ad560445b97a8f4ecab414b
Dario <dario.rucker@yahoo.com>
2023-03-09 12:23:24 +0100

    Initial commit

---
+++ S04T02N03RuckerDario/src/main/java/cat/itacademy/barcelonactiva/rucker/dario/s04/t02/n03/S04T02N03RuckerDarioApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.rucker.dario.s04.t02.n03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N03RuckerDarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N03RuckerDarioApplication.class, args);
+	}
+
+}
